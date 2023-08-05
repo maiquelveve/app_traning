81a5fa652af4b20a54469a73b40dbb0f1ad1ce12
maiquelveve <maiquelveve2008@gmail.com>
2023-08-05 12:06:55 -0300

    feat: terminado os teste do trocar senha

--- back_app_traning/tests/integration/users/changePassword.spec.ts
+++ back_app_traning/tests/integration/users/changePassword.spec.ts
@@ -4,7 +4,7 @@ import { connectionSql } from "../../../src/database/connectionSql";
 import { testServer } from "../../jest-setup";
 import { encryptPassword } from "../../../src/helpers";
 
-describe("@dev", () => {
+describe("@integration", () => {
   describe("USER CONTROLLER - Change Password", () => {
 
     const name = "test";
@@ -26,25 +26,38 @@ describe("@dev", () => {
       connectionSql.close();
     });
 
-    it("it should be possible to updated profile",  async () => {      
+    it("it should not be possible to update the profile with the password incorrect",  async () => {      
       const newResponse = await testServer.put("/users/changePassword")
         .set("authorization", token)
-        .send({ passwordCurrent: "123456", newPassword: "654321" });
-    
-      expect(newResponse.body.isSuccess).toBeTruthy();
-    });
-
-    it("it should not be possible to update the profile with the password invalid",  async () => {      
-      const newResponse = await testServer.put("/users/changePassword")
-        .set("authorization", token)
-        .send({ newPassword: "999999999"  });
+        .send({ passwordCurrent: "999999999", newPassword: "654321" });
     
       expect(newResponse.body.isError).toBeTruthy();
       expect(newResponse.statusCode).toBe(400);
       expect(newResponse.body.errors[0]).toEqual("Senha Invalida.");
     });
 
-    it("it should not be possible to update the profile without the password",  async () => {      
+    it("it should not be possible to update the profile with the new password invalid < 5 caracteres",  async () => {      
+      const newResponse = await testServer.put("/users/changePassword")
+        .set("authorization", token)
+        .send({ passwordCurrent: "999999999", newPassword: "65" });
+    
+      expect(newResponse.body.isError).toBeTruthy();
+      expect(newResponse.statusCode).toBe(400);
+      expect(newResponse.body.errors[0]).toEqual("Nova Senha não pode ser menor que 5 caracteres.");
+    });
+
+    it("it should not be possible to update the profile with the new password invalid with space in nulls",  async () => {      
+      const newResponse = await testServer.put("/users/changePassword")
+        .set("authorization", token)
+        .send({ passwordCurrent: "999999999", newPassword: "        " });
+    
+      expect(newResponse.body.isError).toBeTruthy();
+      expect(newResponse.statusCode).toBe(400);
+      expect(newResponse.body.errors[0]).toEqual("Nova Senha não pode ser menor que 5 caracteres.");
+      expect(newResponse.body.errors[1]).toEqual("Nova Senha é obrigatória.");
+    });
+
+    it("it should not be possible to update the profile without the password current",  async () => {      
       const newResponse = await testServer.put("/users/changePassword")
         .set("authorization", token)
         .send({ newPassword: "654321"  });
@@ -57,11 +70,22 @@ describe("@dev", () => {
     it("it should not be possible to update the profile without the new password",  async () => {      
       const newResponse = await testServer.put("/users/changePassword")
         .set("authorization", token)
-        .send({ password: "123456"  });
+        .send({ passwordCurrent: "123456"  });
     
       expect(newResponse.body.isError).toBeTruthy();
       expect(newResponse.statusCode).toBe(400);
       expect(newResponse.body.errors[0]).toEqual("Nova Senha é obrigatória.");
+    });
+
+    it("it should be possible to updated profile",  async () => {      
+      const newResponse = await testServer.put("/users/changePassword")
+        .set("authorization", token)
+        .send({ passwordCurrent: "123456", newPassword: "654321" });
+    
+      const responseWithNewPassword = await testServer.post("/users/login").send({ email, password: "654321" });
+
+      expect(newResponse.body.isSuccess).toBeTruthy();
+      expect(responseWithNewPassword.body.isSuccess).toBeTruthy();
     });
   });
 });  
