b028e3e468ca5e770464abef0b89b522a4a86520
maiquelveve <maiquelveve2008@gmail.com>
2023-11-12 21:46:59 -0300

    feat: adicionando as rotas

---
+++ mobile_app_training/src/routes/tab.routes.tsx
@@ -0,0 +1,29 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+import Feed from "../screens/Feed";
+import New from "../screens/New";
+
+const Tab = createBottomTabNavigator();
+
+export default function TabRoutes() {
+  return(
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({ size, color }) => <Feather name="home" color={color} size={size }/>,
+          tabBarLabel: "Início"
+        }}
+      />
+      <Tab.Screen
+        name="new"
+        component={New}
+        options={{
+          tabBarIcon: ({ size, color }) => <Feather name="plus" color={color} size={size }/>,
+          tabBarLabel: "Novo"
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
