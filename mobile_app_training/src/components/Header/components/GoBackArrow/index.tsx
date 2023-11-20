4c23baffa33918c659dff313edd1478e5752b4f5
Maiquel Leites <38261904+maiquelveve@users.noreply.github.com>
2023-11-20 13:31:08 -0300

    feat: add goBack no header stack

--- mobile_app_training/src/components/Header/components/GoBackArrow/index.tsx
+++ mobile_app_training/src/components/Header/components/GoBackArrow/index.tsx
@@ -1,10 +1,12 @@
 import { Box } from "native-base";
+import { useNavigation } from "@react-navigation/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export const GoBackArrow: React.FC = () => {
+  const navigation = useNavigation();
   return(
     <Box display="flex" alignItems="flex-start" justifyContent="center" p={2}>
-      <MaterialCommunityIcons name="keyboard-backspace" size={24} color={"white"} />
+      <MaterialCommunityIcons name="keyboard-backspace" size={24} color={"white"} onPress={() => navigation.goBack()} />
     </Box>
   );
 };
